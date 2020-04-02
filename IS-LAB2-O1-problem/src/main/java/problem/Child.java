d1a6a775c402b899465df30b7c52f96d83743dfe
FernanoSolo <csolorznoh98@gmail.com>
2020-04-02 17:57:11 -0600

    Solution IS-LAB2-01 Problem

---
+++ IS-LAB2-O1-problem/src/main/java/problem/Child.java
@@ -0,0 +1,32 @@
+package problem;
+
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Child extends Person {
+
+    /**
+     * Default constructor
+     */
+    public Child() {
+    }
+
+    /**
+     * @param Name 
+     * @param LastName
+     */
+    public void Child(String Name, String LastName) {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void Pay() {
+        // TODO implement here
+    }
+
+}
---
+++ IS-LAB2-O1-problem/src/main/java/problem/Person.java
@@ -0,0 +1,85 @@
+package problem;
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Person {
+
+    /**
+     * Default constructor
+     */
+    public Person() {
+    }
+
+    /**
+     * 
+     */
+    public int DPI;
+
+    /**
+     * 
+     */
+    public String Name;
+
+    /**
+     * 
+     */
+    public String LastName;
+
+    /**
+     * 
+     */
+    public int CredirCard;
+
+    /**
+     * 
+     */
+    public void Pay() {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public int getDPI() {
+        // TODO implement here
+        return 0;
+    }
+
+    /**
+     * @return
+     */
+    public String getName() {
+        // TODO implement here
+        return "";
+    }
+
+    /**
+     * @return
+     */
+    public String getLastName() {
+        // TODO implement here
+        return "";
+    }
+
+    /**
+     * @return
+     */
+    public int getCreditCard() {
+        // TODO implement here
+        return 0;
+    }
+
+    /**
+     * @param DPI 
+     * @param Name 
+     * @param LastName 
+     * @param CreditCard
+     */
+    public void Person(int DPI, String Name, String LastName, int CreditCard) {
+        // TODO implement here
+    }
+
+}
